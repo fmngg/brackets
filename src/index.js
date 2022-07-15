00710cf1d530cd0743d1dbc2f6819ee4e604b167
Ilya <ilyafomin9283@mail.ru>
2022-07-15 20:27:36 +0300

    init: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let resultStack = [];
+  let openElement = 0;
+
+  for (let i = 0, lenS = str.length; i < lenS; i++) {
+    for (const [x, y] of bracketsConfig) {
+      if (str[i] == y) {
+        if (resultStack[openElement - 1] == x) {
+          resultStack.pop();
+          openElement--;
+        } else {
+          resultStack.push(str[i]);
+          openElement++;
+        }
+      } else {
+        if (str[i] == x) {
+          resultStack.push(str[i]);
+          openElement++;
+        }
+      }
+    }
+  }
+
+  return !resultStack.length;
+};
